3a2dae72f3376be894eeb9ea0d00c02d500af416
Emily Lopez <emily.burst@lego.com>
2025-08-18 08:55:03 +0100

    Add formik validation

--- src/components/SendEmail/SendEmail.tsx
+++ src/components/SendEmail/SendEmail.tsx
@@ -1,12 +1,38 @@
 import emailjs from "@emailjs/browser";
-import { Form, Formik, useFormik } from "formik";
+import { ErrorMessage, Field, Form, Formik, useFormik } from "formik";
 import { contactMeValidationSchema } from "../../utils/validation/contactMeFormValidation";
 import styles from "./SendEmail.module.css";
 
 const SERVICE_ID = "official_email";
 const TEMPLATE_ID = "template_j13xxmn";
 
+type HandleOnSubmitProps = {
+	name: string;
+	email: string;
+	message: string;
+};
+
 export const SendEmail = () => {
+	const handleOnSubmit = async (values: HandleOnSubmitProps) => {
+		try {
+			await emailjs.send(
+				SERVICE_ID,
+				TEMPLATE_ID,
+				{
+					title: "Email from website",
+					name: values.name,
+					message: values.message,
+					email: values.email,
+				},
+				{ publicKey: "CDzF8PqEysCmOXSsJ" },
+			);
+			formik.resetForm();
+			console.log("SUCCESS");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	const formik = useFormik({
 		initialValues: {
 			name: "",
@@ -34,49 +60,62 @@ export const SendEmail = () => {
 				console.log(error);
 			}
 		},
+		validationSchema: contactMeValidationSchema(),
 	});
-	return (
-		<form onSubmit={formik.handleSubmit}>
-			<section className={styles.sectionWrapper}>
-				<label className={styles.label} htmlFor="name">
-					Name
-				</label>
-				<input
-					className={styles.fieldInput}
-					id="name"
-					type="text"
-					name="name"
-					onChange={formik.handleChange}
-					value={formik.values.name}
-				/>
 
-				<label className={styles.label} htmlFor="email">
-					Email
-				</label>
-				<input
-					className={styles.fieldInput}
-					id="email"
-					type="email"
-					name="email"
-					onChange={formik.handleChange}
-					value={formik.values.email}
-				/>
-				<label className={styles.label} htmlFor="message">
-					Message
-				</label>
-				<textarea
-					className={styles.areaInput}
-					id="message"
-					name="message"
-					onChange={formik.handleChange}
-					value={formik.values.message}
-				/>
-				<div className={styles.paddingWrapper}>
-					<button className={styles.sendButton} type="submit" value="Send">
-						Submit
-					</button>
-				</div>
-			</section>
-		</form>
+	return (
+		<Formik
+			initialValues={{
+				name: "",
+				email: "",
+				message: "",
+			}}
+			onSubmit={handleOnSubmit}
+			validationSchema={contactMeValidationSchema()}
+		>
+			<Form>
+				<section className={styles.sectionWrapper}>
+					<label className={styles.label} htmlFor="name">
+						Name
+					</label>
+					<Field className={styles.fieldInput} name="name" type="text" />
+					<ErrorMessage
+						className={styles.validationError}
+						component="p"
+						name="name"
+					/>
+
+					<label className={styles.label} htmlFor="email">
+						Email Address
+					</label>
+					<Field className={styles.fieldInput} name="email" type="email" />
+					<ErrorMessage
+						className={styles.validationError}
+						component="p"
+						name="email"
+					/>
+
+					<label className={styles.label} htmlFor="message">
+						Message
+					</label>
+					<Field
+						className={styles.areaInput}
+						name="message"
+						type="text"
+						as="textarea"
+					/>
+					<ErrorMessage
+						className={styles.validationError}
+						component="p"
+						name="message"
+					/>
+					<div className={styles.paddingWrapper}>
+						<button className={styles.sendButton} type="submit" value="Send">
+							Submit
+						</button>
+					</div>
+				</section>
+			</Form>
+		</Formik>
 	);
 };
--- src/utils/validation/contactMeFormValidation.ts
+++ src/utils/validation/contactMeFormValidation.ts
@@ -1,25 +1,40 @@
-import { type ObjectSchema, object, string } from "yup";
+import { type AnyObject, type ObjectSchema, object, string } from "yup";
 
 const textRegExp = /^[-'\p{L} _@.,]+$/u;
 
 const defaultFormRules = () => {
 	return {
-		name: string().required().min(1).max(35).matches(textRegExp, "pls match"),
+		name: string()
+			.required("This is a requried field")
+			.min(1)
+			.max(35)
+			.matches(textRegExp, "Please enter letters only"),
 		email: string()
-			.required()
+			.required("This is a requried field")
 			.matches(
 				/^\w+([+]?([.-]?\w+))*@\w+([.-]?\w+)*(\.\w{2,})+$/,
-				"sort it out",
+				"Please enter a valid email address",
 			),
-		message: string()
-			.required()
-			.min(1)
-			.max(500)
-			.matches(textRegExp, "pls match"),
+		message: string().required("This is a requried field").min(1).max(500),
 	};
 };
 
-export const contactMeValidationSchema = (): ObjectSchema<any> => {
+type FormRules = ObjectSchema<
+	{
+		name: string;
+		email: string;
+		message: string;
+	},
+	AnyObject,
+	{
+		name: undefined;
+		email: undefined;
+		message: undefined;
+	},
+	""
+>;
+
+export const contactMeValidationSchema = (): FormRules => {
 	const defaultRules = defaultFormRules();
 	return object().shape({ ...defaultRules });
 };
